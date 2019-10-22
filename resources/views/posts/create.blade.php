d0408179dd3201a7a91ff92d165c32b91c5b05cf
simple-man3 <kurganov517@gmail.com>
2019-10-22 15:51:50 +0400

    вынес виджет в отдельный view и вызвал его с помощью @include

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -105,18 +105,6 @@
             <div class="col-12">
                 <post-editor store-link="{{ route('post.store') }}" author="{{ Auth::user()->name }}"></post-editor>
             </div>
-{{--            <div class="col-4">--}}
-{{--                <div class="card mb-2">--}}
-{{--                    <div class="card-body">--}}
-{{--                        <h5 class="card-title">{{ __('Виджетов пока нет') }}</h5>--}}
-{{--                        @auth--}}
-{{--                            <div class="card-text">--}}
-{{--                                <a href="#" class="btn btn-primary">Кнопка добавления нового виджета</a>--}}
-{{--                            </div>--}}
-{{--                        @endauth--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
         </div>
     </div>
 @endsection
