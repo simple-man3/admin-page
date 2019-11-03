7f222cae515a1f8c5a49640a00a07be07ac4ce21
simple-man3 <kurganov517@gmail.com>
2019-11-03 16:51:42 +0400

    модифицировал контроллер, а конкретнее его упростил

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -2,13 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Config;
-use Faker\Provider\Image;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-use mysql_xdevapi\Exception;
 use View;
 use function Psy\debug;
 
@@ -31,37 +28,23 @@ class AdminController extends Controller
 
     public function show_setting()
     {
-        try
+        if(file_exists(resource_path('/views/template/description.json')) && View::exists('template.header') && file_exists(public_path('/img/screen.jpg')))
         {
-            $error=0;
-            $json_data="";
+            $json_data=json_decode(file_get_contents(resource_path("/views/template/description.json")),true);
 
-            if(file_exists(resource_path('/views/template/description.json')) && View::exists('template.header') && file_exists(public_path('/img/screen.jpg')))
+            if(array_key_exists('theme',$json_data) && array_key_exists('author',$json_data) && array_key_exists('desc',$json_data))
             {
-                $error=0;
-                $json_data=json_decode(file_get_contents(resource_path("/views/template/description.json")),true);
-
                 return view('admin_page.admin_setting',[
                     'name_theme'=>$json_data['theme'],
                     'name_author'=>$json_data['author'],
                     'description'=>$json_data['desc'],
-                    'error'=>$error
-                ]);
-            }else{
-                //если нет таких файлов
-                $error=1;
-                return view('admin_page.admin_setting',[
-                    'error'=>$error
+                    'error'=>false
                 ]);
             }
-
-        }catch (\Exception  $e)
-        {
-            //если есть ошибка в коде json
-            $error=1;
-            return view('admin_page.admin_setting',[
-                'error'=>$error
-            ]);
         }
+        //если нет таких файлов
+        return view('admin_page.admin_setting',[
+            'error'=>true
+        ]);
     }
 }
